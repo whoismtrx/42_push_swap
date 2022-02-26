b82bb84a7c99477570a4ac543c836d70ebce70f9
MITIRIX <ghoufal1337@gmail.com>
2022-02-26 22:44:05 +0100

    push swap

--- ft_algo_swap.c
+++ ft_algo_swap.c
@@ -6,7 +6,7 @@
 /*   By: orekabe <orekabe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 23:36:19 by orekabe           #+#    #+#             */
-/*   Updated: 2022/02/25 02:19:15 by orekabe          ###   ########.fr       */
+/*   Updated: 2022/02/26 17:20:32 by orekabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,54 +93,133 @@ t_stack	ft_algo_swap(t_stack stack)
 	// 	// break;
 	// }
 	
+	// while (ft_check_if_not_sorted(stack) || stack.head_b != stack.len)
+	// {
+	// 	int	size_b = 0;
+	// 	while (ft_check_if_not_sorted(stack))
+	// 	{
+	// 		if (stack.b[stack.head_b] < stack.b[stack.last_b] && size_b > 1)
+	// 			stack = ft_rotate_b(stack);
+	// 		else if (stack.b[stack.head_b] < stack.b[stack.head_b + 1] && size_b > 1)
+	// 			stack = ft_swap_b(stack);
+	// 		else if (stack.a[stack.head_a] > stack.a[stack.last_a])
+	// 			stack = ft_rotate_a(stack);
+	// 		else if ((stack.a[stack.head_a] > stack.b[stack.head_b + 1]) || stack.a[stack.head_a] < stack.b[stack.last_b])
+	// 		{
+	// 			stack = ft_push_b(stack);
+	// 			size_b++;
+	// 		}
+	// 		else if (size_b < 2)
+	// 		{
+	// 			stack = ft_push_b(stack);
+	// 			size_b++;
+	// 		}
+	// 		else
+	// 			break;
+	// 	}
+	// 	long stop;
+	// 	long back = stack.a[stack.last_a];
+	// 	int i = stack.last_a;
+	// 		while (stack.a[i] > stack.a[i - 1] && i > 0)
+	// 			i--;
+	// 		if (i == 0)
+	// 			stop = stack.a[i];
+	// 		stop = stack.a[i - 1];
+	// 	while (stack.head_b < stack.len)
+	// 	{
+	// 		if (stack.a[stack.last_a] > stack.b[stack.head_b] && stack.a[stack.last_a] != stop)
+	// 		{
+	// 			stack = ft_rev_rot_a(stack);
+	// 		}
+	// 		else if (stack.b[stack.head_b] > stack.a[stack.last_a])
+	// 			stack = ft_push_a(stack);
+	// 		else if (stack.b[stack.head_b] < stack.a[stack.head_a])
+	// 			stack = ft_push_a(stack);
+	// 	}
+	// 	while (stack.a[stack.last_a] != back)
+	// 		stack = ft_rev_rot_a(stack);
+	// 	// ft_print_stack_a(stack);
+	// 	// ft_print_stack_b(stack);
+	// }
+	
+	long	stop;
+	long	back;
 	while (ft_check_if_not_sorted(stack) || stack.head_b != stack.len)
 	{
-		int	size_b = 0;
 		while (ft_check_if_not_sorted(stack))
 		{
-			if (stack.b[stack.head_b] < stack.b[stack.last_b] && size_b > 1)
-				stack = ft_rotate_b(stack);
-			else if (stack.b[stack.head_b] < stack.b[stack.head_b + 1] && size_b > 1)
+			if (stack.b[stack.head_b] < stack.b[stack.head_b + 1] && stack.head_b < stack.last_b)
 				stack = ft_swap_b(stack);
+			else if (stack.b[stack.head_b] < stack.b[stack.last_b] && stack.head_b < stack.last_b)
+				stack = ft_rotate_b(stack);
+			else if (stack.a[stack.head_a] < stack.a[stack.last_a] && stack.a[stack.head_a] > stack.b[stack.head_b + 1])
+				stack = ft_push_b(stack);
+			else if (stack.a[stack.head_a + 1] > stack.a[stack.head_a] && stack.a[stack.head_a + 1] > stack.a[stack.last_a])
+				stack = ft_swap_a(stack);
+			else if (stack.a[stack.head_a + 1] > stack.a[stack.head_a] && stack.a[stack.head_a + 1] < stack.b[stack.last_b] && stack.head_b < stack.last_b)
+				stack = ft_swap_a(stack);
+			else if (stack.a[stack.head_a + 1] > stack.a[stack.head_a] && stack.a[stack.head_a + 1] > stack.b[stack.head_b + 1] && stack.head_b < stack.last_b)
+				stack = ft_swap_a(stack);
 			else if (stack.a[stack.head_a] > stack.a[stack.last_a])
 				stack = ft_rotate_a(stack);
-			else if ((stack.a[stack.head_a] > stack.b[stack.head_b + 1]) || stack.a[stack.head_a] < stack.b[stack.last_b])
-			{
+			else if (stack.a[stack.head_a] < stack.a[stack.last_a] && stack.head_b == stack.len)
 				stack = ft_push_b(stack);
-				size_b++;
-			}
-			else if (size_b < 2)
-			{
+			else if (stack.a[stack.head_a] < stack.a[stack.last_a] && stack.a[stack.head_a] > stack.b[stack.head_b])
 				stack = ft_push_b(stack);
-				size_b++;
-			}
 			else
 				break;
 		}
-		long stop;
-		long back = stack.a[stack.last_a];
+		if (!ft_check_if_not_sorted(stack))
+			break;
 		int i = stack.last_a;
-			while (stack.a[i] > stack.a[i - 1] && i > 0)
-				i--;
-			if (i == 0)
-				stop = stack.a[i];
-			stop = stack.a[i - 1];
-		while (stack.head_b < stack.len)
+		while (stack.a[i] > stack.a[i - 1] && i > 0)
+			i--;
+		if (i == 0)
+			stop = stack.a[i];
+		stop = stack.a[i - 1];
+		back = stack.a[stack.last_a];
+		while (stack.a[stack.last_a] != stop || stack.head_b < stack.len)
 		{
-			if (stack.a[stack.last_a] > stack.b[stack.head_b] && stack.a[stack.last_a] != stop)
-			{
-				stack = ft_rev_rot_a(stack);
-			}
-			else if (stack.b[stack.head_b] > stack.a[stack.last_a])
+			if (stack.b[stack.head_b] > stack.a[stack.last_a])
 				stack = ft_push_a(stack);
+			else if (stack.a[stack.last_a] == stop)
+				stack = ft_push_a(stack);
+			else
+				stack = ft_rev_rot_a(stack);
+		}
+		while (stack.a[stack.last_a] != back || stack.a[stack.head_a] > stack.a[stack.head_a + 1])
+		{
+			if (stack.b[stack.head_b] > stack.b[stack.last_b] && stack.head_b < stack.last_b)
+				stack = ft_rotate_b(stack);
+			else if (stack.b[stack.head_b] > stack.b[stack.head_b + 1] && stack.head_b < stack.last_b)
+				stack = ft_swap_b(stack);
+			else if (stack.a[stack.last_a] == back && stack.a[stack.head_a] > stack.a[stack.head_a + 1])
+				stack = ft_push_b(stack);
+			else if (stack.a[stack.last_a] > stack.a[stack.head_a] && stack.a[stack.last_a] > stack.a[stack.last_a - 1])
+				stack = ft_rev_rot_a(stack);
+			else if (stack.a[stack.last_a] > stack.a[stack.head_a])
+				stack = ft_rev_rot_a(stack);
+			else if (stack.a[stack.head_a] > stack.a[stack.head_a + 1])
+				stack = ft_push_b(stack);
+			else
+				break;
+		}
+		long last = stack.a[stack.last_a];
+		while (stack.last_a + 1 < stack.len)
+		{
+			if (stack.a[stack.head_a] < stack.b[stack.head_b] && stack.a[stack.head_a] != last)
+				stack = ft_rotate_a(stack);
 			else if (stack.b[stack.head_b] < stack.a[stack.head_a])
 				stack = ft_push_a(stack);
+			else break;
 		}
-		while (stack.a[stack.last_a] != back)
-			stack = ft_rev_rot_a(stack);
-		// ft_print_stack_a(stack);
-		// ft_print_stack_b(stack);
+		while (stack.head_b < stack.len)
+		{
+			if (stack.a[stack.head_a] > stack.a[stack.last_a])
+				stack = ft_rotate_a(stack);
+			else if (stack.a[stack.head_a] < stack.b[stack.head_b])
+				stack = ft_push_a(stack);
+		}
 	}
-	
 	return (stack);
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: orekabe <orekabe@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 23:00:36 by orekabe           #+#    #+#             */
-/*   Updated: 2022/02/25 00:13:07 by orekabe          ###   ########.fr       */
+/*   Updated: 2022/02/26 21:09:28 by orekabe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	main(int argc, char **argv)
 	// ft_print_stack_a(stack);
 	// ft_print_stack_b(stack);
 	stack = ft_algo_swap(stack);
-	// ft_print_stack_a(stack);
-	// ft_print_stack_b(stack);
-	// ft_rev_stack("841 -518 -302 749 -430 378 170 -643 -607 347 36 -943");
+	ft_print_stack_a(stack);
+	ft_print_stack_b(stack);
+	// ft_rev_stack("71 14 309 45 104 52 86 107 804 61 34 42 55 21 41 23 87 12 33 0");
 }
